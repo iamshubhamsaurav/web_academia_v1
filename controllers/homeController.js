499d46c0cc2ae014f95dd6e4aa932fc41ff0ae76
Shubham  Saurav <shubhamsinha186@gmail.com>
2023-06-14 00:06:09 +0530

    Home page: questions templating done

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,15 @@
-const catchAsync = require("../utils/catchAsync");
+const catchAsync = require("../utils/catchAsync")
+const Question = require('../models/Question')
 
 exports.getHome = catchAsync(async (req, res, next) => {
-    res.render('home/index')
+    const questions = await Question.find().populate({
+        path: 'answers',
+        select: 'text _id createdAt'
+    })
+    // questions.forEach(question => {
+    //     console.log(new Date(question.answers.slice(-1)[0].createdAt).getHours())
+    // });
+    res.render('home/index', {questions})
 })
 
 
